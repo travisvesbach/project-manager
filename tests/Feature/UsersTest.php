3522e5bd8b5268247eed355979a049b7f7f77167
Travis Vesbach <tav1990@hotmail.com>
2021-01-30 20:10:19 -0600

    Add missing users tests

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -28,15 +28,20 @@ class UsersTest extends TestCase
     public function guests_cannot_manage_users() {
         $this->get('/users')->assertRedirect('login');
         $this->get('/users/create')->assertRedirect('login');
-        $this->post('users', [
+        $this->post('users', $attributes = [
                 'name' => $this->faker->name,
                 'email' => $this->faker->unique()->safeEmail,
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'role' => 'User',
             ])
             ->assertRedirect('login');
-
         $this->assertCount(0, User::all());
+
+        $other_user = User::factory()->create();
+        $this->patch('users/' . $other_user->id, $attributes)->assertRedirect('login');
+        $this->delete('users/' . $other_user->id)->assertRedirect('login');
+        $this->assertCount(1, User::all());
     }
 
     /** @test **/
@@ -46,15 +51,20 @@ class UsersTest extends TestCase
 
         $this->get('/users')->assertStatus(403);
         $this->get('/users/create')->assertStatus(403);
-        $this->post('users', [
+        $this->post('users', $attributes = [
                 'name' => $this->faker->name,
                 'email' => $this->faker->unique()->safeEmail,
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'role' => 'User',
             ])
             ->assertStatus(403);
-
         $this->assertCount(2, User::all());
+
+        $other_user = User::factory()->create();
+        $this->patch('users/' . $other_user->id, $attributes)->assertStatus(403);
+        $this->delete('users/' . $other_user->id)->assertStatus(403);
+        $this->assertCount(3, User::all());
     }
 
     /** @test **/
@@ -65,8 +75,28 @@ class UsersTest extends TestCase
             ->post('users', [
                 'name' => $this->faker->name,
                 'email' => $this->faker->unique()->safeEmail,
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'role' => 'User',
+            ])
+            ->assertRedirect('users');
+
+        $this->assertCount(2, User::all());
+    }
+
+    /** @test **/
+    public function an_admin_can_update_users() {
+        $admin_user = User::factory()->create();
+
+        $user_to_update = User::factory()->create();
+
+        $this->actingAs($admin_user)
+            ->patch('users/' . $user_to_update->id, [
+                'name' => $this->faker->name,
+                'email' => $this->faker->unique()->safeEmail,
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'role' => 'User'
             ])
             ->assertRedirect('users');
 
